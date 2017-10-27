7293df667ed1542c0881e9db41cbf5f988b5dff1
Lava <lavatahir@gmail.com>
2017-10-26 20:39:01 -0400

    updated css, following angular now

--- SECP-service/src/main/resources/assets/app/js/home/home-controller.js
+++ SECP-service/src/main/resources/assets/app/js/home/home-controller.js
@@ -1,15 +1,4 @@
 angular.module('SECP')
     .controller('HomeController', ['$scope', function ($scope) {
-
-        $scope.displayLoginForm = function () {
-            $scope.getElementById('register').className = 'hiddenForm';
-            $scope.getElementById('login').classList.remove('hiddenForm');
-            console.log('hi');
-        };
-
-        $scope.displayRegisterForm = function () {
-            $scope.getElementById('login').className = 'hiddenForm';
-            $scope.getElementById('register').classList.remove('hiddenForm');
-            console.log('hi2');
-        };
+        $scope.show = "loginForm";
     }]);
