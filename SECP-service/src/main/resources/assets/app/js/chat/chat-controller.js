2c5ffd52a9ac0cb2ead6d1bab596482344939b34
Mohamed Dahrouj <dahrouj.m@gmail.com>
2017-11-25 21:13:45 -0500

    [Issue #34] Removed unnecessary functions in chat-controller.js

--- SECP-service/src/main/resources/assets/app/js/chat/chat-controller.js
+++ SECP-service/src/main/resources/assets/app/js/chat/chat-controller.js
@@ -38,65 +38,6 @@ angular.module('SECP')
             $scope.messages = data;
          });
       };
-
-      $scope.create = function () {
-        $scope.clear();
-        $scope.open();
-      };
-
-      $scope.update = function (id) {
-        $scope.chat = Chat.get({id: id});
-        $scope.open(id);
-      };
-
-      $scope.delete = function (id) {
-        Chat.delete({id: id},
-          function () {
-            $scope.chats = Chat.query();
-          });
-      };
-
-      $scope.save = function (id) {
-        if (id) {
-          Chat.update({id: id}, $scope.chat,
-            function () {
-              $scope.chats = Chat.query();
-              $scope.clear();
-            });
-        } else {
-          Chat.save($scope.chat,
-            function () {
-              $scope.chats = Chat.query();
-              $scope.clear();
-            });
-        }
-      };
-
-      $scope.clear = function () {
-        $scope.chat = {
-
-          "myattr": "",
-
-          "id": ""
-        };
-      };
-
-      $scope.open = function (id) {
-        var chatSave = $modal.open({
-          templateUrl: 'chat-save.html',
-          controller: 'ChatSaveController',
-          resolve: {
-            chat: function () {
-              return $scope.chat;
-            }
-          }
-        });
-
-        chatSave.result.then(function (entity) {
-          $scope.chat = entity;
-          $scope.save(id);
-        });
-      };
     }])
   .controller('ChatSaveController', ['$scope', '$modalInstance', 'chat',
     function ($scope, $modalInstance, chat) {
