fa7487530202627e3db68a18ba210d933c0d134f
Mohamed Dahrouj <dahrouj.m@gmail.com>
2017-12-06 11:33:25 -0500

    Merged master into adminportal
    Conflicts: bower.json and app.js

--- SECP-service/src/main/resources/assets/app/js/login/login-controller.js
+++ SECP-service/src/main/resources/assets/app/js/login/login-controller.js
@@ -1,3 +1,13 @@
 angular.module('SECP')
-    .controller('LoginController', ['$scope', function ($scope) {
+    .controller('LoginController', ['$scope', 'Auth', '$location', function ($scope, Auth, $location) {
+        $scope.login = function () {
+            Auth.login($scope.user).then(function(res) {
+                if(res.status == 200) {
+                    swal('Welcome to SECP!', 'Login successful!','success');
+                    $location.path('/chats');
+                } else {
+                    swal('Oops..!', res.data.message,'error');
+                }
+            });
+        }
     }]);
