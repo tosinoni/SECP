3e0fddeb812e746eeb8ef1605df757946c4f5f43
alifarah <alifarah613@gmail.com>
2017-12-31 12:50:53 -0500

    created messageDAO and added it to the chatsockethandler #18

---
+++ SECP-service/src/main/java/com/visucius/secp/daos/MessageDAO.java
@@ -0,0 +1,37 @@
+package com.visucius.secp.daos;
+
+import com.google.common.base.Optional;
+import com.visucius.secp.models.Message;
+import io.dropwizard.hibernate.AbstractDAO;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class MessageDAO extends AbstractDAO<Message> {
+
+    public MessageDAO(SessionFactory provider) {
+        super(provider);
+    }
+
+
+    public Optional<Message> find(long id) {
+        return Optional.fromNullable(get(id));
+    }
+
+    public List<Message> findAll() {
+        return (List<Message>) criteria().list();
+    }
+
+    public Message save(Message entity) throws HibernateException {
+        return persist(entity);
+    }
+
+    public Message merge(Message entity) throws HibernateException {
+        return (Message) currentSession().merge(entity);
+    }
+
+    public void delete(Message entity) throws HibernateException {
+        currentSession().delete(entity);
+    }
+}
