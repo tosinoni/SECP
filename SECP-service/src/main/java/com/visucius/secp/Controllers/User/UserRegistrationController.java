ae8f36d263c3258a65d663f2af7cc91ee04d231e
VinV5 <vincentvu@cmail.carleton.ca>
2018-01-13 17:33:24 -0500

    minor fix

--- SECP-service/src/main/java/com/visucius/secp/Controllers/User/UserRegistrationController.java
+++ SECP-service/src/main/java/com/visucius/secp/Controllers/User/UserRegistrationController.java
@@ -44,7 +44,7 @@ public class UserRegistrationController{
             try {
                 String hashPassword = PasswordUtil.createHash(request.password);
                 User user = new User(request.firstName, request.lastName, request.userName, request.email, hashPassword);
-                user.setAvatar_url(request.avatar_url);
+                user.setAvatarUrl(request.avatar_url);
                 user.setDisplayName(request.displayName);
                 User createdUser = userDAO.save(user);
                 return new UserRegistrationResponse(true, UserErrorMessage.USER_CREATED, Response.Status.CREATED, errors, createdUser.getId());
