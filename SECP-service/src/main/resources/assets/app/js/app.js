ff1ff563b705c1e81363b1c0aaa5144d381012c2
Tosin <tosin.oni@carleton.ca>
2018-01-24 00:25:10 -0500

    [Issue #16] removed angular nice scrooll

--- SECP-service/src/main/resources/assets/app/js/app.js
+++ SECP-service/src/main/resources/assets/app/js/app.js
@@ -1,6 +1,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('SECP', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date',
-    'routeStyles', 'angular-jwt', 'datatables', 'angular-nicescroll'])
+    'routeStyles', 'angular-jwt', 'datatables'])
   .config(function ($routeProvider, $locationProvider, jwtOptionsProvider, $httpProvider) {
       localforage.setDriver([localforage.WEBSQL, localforage.INDEXEDDB]);
 
