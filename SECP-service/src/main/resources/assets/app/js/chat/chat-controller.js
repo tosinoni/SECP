e931d6edff5082ea0b29ad5808bd493d52b15e11
Mohamed Dahrouj <dahrouj.m@gmail.com>
2018-01-28 15:36:56 -0500

    [Issue #130] Implemented filter processing for permissions and roles

--- SECP-service/src/main/resources/assets/app/js/chat/chat-controller.js
+++ SECP-service/src/main/resources/assets/app/js/chat/chat-controller.js
@@ -36,45 +36,70 @@ angular.module('SECP')
         }
       });
 
-      $scope.sendMessageOnceFiltered = function(){
-          var messageDTO = {
-              groupId: $scope.selectedChat.groupID,
-              senderId: $scope.currentUser.userID,
-              body: $scope.messageInput,
-              reason: "message",
-              timestamp: moment()
-          };
+      $scope.sendMessage = function(){
+          if($scope.messageContainsFilterWord()) {
+              var messageDTO = {
+                  groupId: $scope.selectedChat.groupID,
+                  senderId: $scope.currentUser.userID,
+                  body: $scope.messageInput,
+                  reason: "message",
+                  timestamp: moment()
+              };
 
-          Socket.send(messageDTO);
-          $scope.messages.push(messageDTO)
-          setLastMessageForContacts(messageDTO.groupId, messageDTO);
-          //clearing the message input in the textarea
-          $scope.messageInput = null;
+              Socket.send(messageDTO);
+              $scope.messages.push(messageDTO)
+              setLastMessageForContacts(messageDTO.groupId, messageDTO);
+              //clearing the message input in the textarea
+              $scope.messageInput = null;
+          }
         }
 
-      $scope.sendMessage = function() {
-          let filterWords = Admin.getAllFilters();
-          //Concat the permission levels and roles of the sender and receiver into one array
-          console.log(Admin.getUser($scope.currentUser.userID));
-          let tags = Admin.getUser($scope.currentUser.userID).permissions.concat(Admin.getUser($scope.currentUser.userID).roles);
-          let receiverTags = Admin.getGroup($scope.selectedChat.groupID).permissions.concat(Admin.getGroup($scope.selectedChat.groupID).roles);
-          tags.concat(receiverTags);
-          let messageContent = $scope.messageInput;
-          let canSendMessage = true;
-          for(let filterWord in filterWords){
-              if(messageContent.includes(filterWord)){
-                  canSendMessage = false;
-                  swal(
-                      'Oops...',
-                      'Could not deliver message due to sensitive content!',
-                      'error'
-                  )
-                  break;
+      $scope.messageContainsFilterWord = function() {
+          let senderPermission = $scope.currentUser.permission
+          let senderRoles = $scope.currentUser.roles;
+          let receiverPermissions = $scope.selectedChat.permissions;
+          let receiverRoles = $scope.selectedChat.roles;
+          //Split message input on space or comma
+          let messageWords = $scope.messageInput.split(/[ ,]+/);
+          Admin.getAllFilters().then( function (filterWordsSet) {
+              if(filterWordsSet) {
+                  var filterWords = {};
+                  //Create map of filter words and names
+                  for(let filterWord of filterWordsSet){
+                      filterWords[filterWord.name]=filterWord;
+                  }
+                  //let filterWords = filterWordsSet.map(filterWord => filterWord.name);
+                  for (let messageWord of messageWords) {
+                      if (filterWords[messageWord]) {
+                          if (!$scope.messageSendable(filterWords[messageWord].permissions, filterWords[messageWord].roles, senderPermission, senderRoles, receiverPermissions, receiverRoles)) {
+                              swal(
+                                  'Oops...',
+                                  'Could not deliver message due to sensitive content!',
+                                  'error'
+                              )
+                              return true;
+                          }
+                      }
+                  }
+              }
+          });
+          return false;
+      };
+      $scope.messageSendable = function(filterPermissions, filterRoles, senderPermission, senderRoles, receiverPermissions, receiverRoles){
+          //Check to see if the sender permission is part of the allowed filter permissions
+          if(filterPermissions.contains(senderPermission)){
+              //Perform the intersection of senderRoles and filterRoles
+              if(senderRoles.filter((role) => filterRoles.includes(role))){
+                  //Check to see if receiverPermissions is subset of filterPermissions
+                  if(receiverPermissions.some((permission) => filterPermissions.indexOf(permission)===-1)){
+                      //Check to see if receiverRoles is subset of filterRoles
+                      if(receiverRoles.some((permission) => filterRoles.indexOf(permission)===-1)){
+                          return true;
+                      }
+                  }
               }
           }
-          if(canSendMessage){
-            $scope.sendMessageOnceFiltered();
-          }
+          return false;
       };
 
         $scope.sendMessageUsingEnter = function(event) {
