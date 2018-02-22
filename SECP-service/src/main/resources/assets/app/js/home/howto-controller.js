e71a0f86d611be18b1dec9292483cc9136570abe
Mohamed Dahrouj <dahrouj.m@gmail.com>
2018-02-22 01:28:52 -0500

    [Issue #134] Update js to toggle plus and minus and misc text changes to docs page

--- SECP-service/src/main/resources/assets/app/js/home/howto-controller.js
+++ SECP-service/src/main/resources/assets/app/js/home/howto-controller.js
@@ -1,11 +1,16 @@
 angular.module('SECP')
   .controller('HowToController', ['$scope', function ($scope) {
       $(document).ready(function(){
+          // Add minus icon for collapse element which is open by default
+          $(".collapse.in").each(function(){
+              $(this).siblings(".panel-heading").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+          });
+
           // Toggle plus minus icon on show hide of collapse element
           $(".collapse").on('show.bs.collapse', function(){
-              $(this).parent().find(".glyphicon").removeClass("glyphicon-plus").addClass("fa fa-minus");
+              $(this).parent().find(".fa").removeClass("fa-plus").addClass("fa-minus");
           }).on('hide.bs.collapse', function(){
-              $(this).parent().find(".glyphicon").removeClass("fa fa-minus").addClass("glyphicon-plus");
+              $(this).parent().find(".fa").removeClass("fa-minus").addClass("fa-plus");
           });
       });
   }]);
