7205242ebcd921dcd215b306b61ec560b8f0c0dd
Lava <lavatahir@gmail.com>
2018-02-21 20:38:15 -0500

    Didnt realize these were already done. All thats left is the server side implementation.

--- SECP-service/src/main/resources/assets/app/js/password/password-controller.js
+++ SECP-service/src/main/resources/assets/app/js/password/password-controller.js
@@ -2,7 +2,7 @@ angular.module('SECP')
     .controller('PasswordController', ['$scope', '$window', function ($scope, $window) {
         $scope.password_conf = null;
         $scope.sendEmail = function () {
-            swal('email sent.');    //TO DO ADD SERVER SIDE LOGIC TO SEND EMAIL
+            swal('Email Sent.');    //TO DO ADD SERVER SIDE LOGIC TO SEND EMAIL
         }
     }]);
     
